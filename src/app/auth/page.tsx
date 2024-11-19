9e5168dfeb2ff27cbc06113545d212829a6b3a80
BrianTaylor <bataylor135@gmail.com>
2024-11-19 16:35:24 -0500

    Updated auth page.

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+'use client';
+
+import React, { useContext } from 'react';
+import { WeatherDataContext } from '@/context/WeatherDataContext';
 import AuthForm from '@/components/AuthForm';
 
 const AuthPage = () => {
+  const { weatherData } = useContext(WeatherDataContext);
+
+  const locationName = weatherData?.location?.name;
+
   return (
     <div className="container mx-auto px-4">
-      <h1 className="text-3xl font-bold text-center my-8">Welcome to WeatherWise</h1>
+      <p className="text-3xl text-center my-8">
+      Weather can be unpredictable, but we have your back.
+      </p>
+      <p className="text-xl text-center my-8">
+        {locationName 
+          ? `Sign in to stay updated in ${locationName}!`
+          : 'Sign in to stay updated in your area!'}
+      </p>
       <AuthForm />
     </div>
   );
